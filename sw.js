e1e45e0b9ccf265068efcf70a3042ea962b871cd
Klettverschluss <klettjonas2@gmail.com>
2025-12-25 21:00:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,7 @@
-const CACHE = "planer-v1";
-const ASSETS = [
-  "/Kalender/",
-  "/Kalender/index.html",
-  "/Kalender/manifest.webmanifest",
-  "/Kalender/icon-192.png",
-  "/Kalender/icon-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
